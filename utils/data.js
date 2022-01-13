d607bf197aa8c33780219368e62720fe30cd05c8
Saransh Kumar <cryptosingh@gmail.com>
2022-01-13 21:15:52 +0530

    updated Checkstyle date.

--- utils/data.js
+++ utils/data.js
@@ -1,7 +1,7 @@
 const line = ["Currently I'm learning Spring and working on my DSA skills and projects.", "I am passionate about Computer Science and want to contribute to Computer Science community by creating something new and unique.", "I believe in smart work and anyone with the curiosity and want to learn and create something can excel in Computer Science field. At the age of 6, I was introduced to Computers since then I love Computers and curiosity inside me keep me motivated to learn something new in this field. Recently, I dive into the ocean of web development and I like back-end development interesting. So, I want to continue my career as a Back-end Developer."];
 const work_experience = [
 	{
-		date: "Mar 2021 – May 2021",
+		date: "Mar 2021 - May 2021",
 		org: "Checkstyle",
 		org_link: "https://github.com/checkstyle/checkstyle",
 		position: "Contributor",
