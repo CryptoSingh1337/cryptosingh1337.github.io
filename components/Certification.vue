1d7a2baca287d2ba1476e79eaf09feebad0dd9ad
Saransh Kumar <lunaticdevs1337@gmail.com>
2023-09-13 00:30:35 +0530

    Change text color

--- components/Certification.vue
+++ components/Certification.vue
@@ -5,7 +5,7 @@
       <span class="block">{{ certification.organization }}</span>
     </div>
     <div>
-      <a v-if="certification.url" class="hover:underline" :href="certification.url" target="_blank">
+      <a v-if="certification.url" class="text-gray-500 hover:underline" :href="certification.url" target="_blank">
         {{ certification.title }}
         <svg class="inline" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="10" height="10" viewBox="0 0 24 24"
           style="fill: var(--dark)">
@@ -13,7 +13,7 @@
             d="M 19.980469 2.9902344 A 1.0001 1.0001 0 0 0 19.869141 3 L 15 3 A 1.0001 1.0001 0 1 0 15 5 L 17.585938 5 L 8.2929688 14.292969 A 1.0001 1.0001 0 1 0 9.7070312 15.707031 L 19 6.4140625 L 19 9 A 1.0001 1.0001 0 1 0 21 9 L 21 4.1269531 A 1.0001 1.0001 0 0 0 19.980469 2.9902344 z M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 13 A 1.0001 1.0001 0 1 0 19 13 L 19 19 L 5 19 L 5 5 L 11 5 A 1.0001 1.0001 0 1 0 11 3 L 5 3 z" />
         </svg>
       </a>
-      <div v-else class="hover:underline">
+      <div v-else class="text-gray-500">
         {{ certification.title }}
       </div>
     </div>
