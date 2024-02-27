b5dab430785b52f21f6327caf1a749fa45f87c9f
Saransh Kumar <lunaticdevs1337@gmail.com>
2024-02-27 22:16:31 +0530

    Update certification title to courses

--- components/section/Certification.vue
+++ components/section/Certification.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="m-8 xl:w-64 xl:mr-0 mt-0 ">
-    <h3 class="mb-4 font-bold text-lg text-black dark:text-white duration-700">Certifications</h3>
+    <h3 class="mb-4 font-bold text-lg text-black dark:text-white duration-700">Courses</h3>
     <ul class="pl-0 text-xs md:text-sm xl:hidden">
       <div class="sm:divide-y divide-solid divide-primary-200">
         <li class="grid gap-4 grid-cols-[1fr_1fr_3fr] items-center pb-3 text-primary-400 font-semibold">
