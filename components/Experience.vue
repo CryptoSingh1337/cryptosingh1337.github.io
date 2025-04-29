ce702d5024c67e30bf8aa95be5c841e545f03040
Saransh Kumar <lunaticdevs1337@gmail.com>
2025-04-29 21:58:26 +0530

    Fix anchor tag

--- components/Experience.vue
+++ components/Experience.vue
@@ -2,17 +2,17 @@
   <div :class="['work-experience-block', isLeft ? classLeft[0] : classRight[0]]">
     <span :class="['work-experience-period', isLeft ? classLeft[1] : classRight[1]]">{{ experience.date }}</span>
     <div :class="['work-experience-info', isLeft ? classLeft[2] : classRight[2]]">
-      <a class="work-experience-link font-bold text-base hover:underline" :href="experience.organizationUrl"
-        target="_blank">
-        <h3>
+      <h3>
+        <a class="work-experience-link font-bold text-base hover:underline" :href="experience.organizationUrl"
+          target="_blank">
           {{ experience.organization }}
           <svg class="inline" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="10" height="10"
             viewBox="0 0 24 24" style="fill: var(--dark)">
             <path
               d="M 19.980469 2.9902344 A 1.0001 1.0001 0 0 0 19.869141 3 L 15 3 A 1.0001 1.0001 0 1 0 15 5 L 17.585938 5 L 8.2929688 14.292969 A 1.0001 1.0001 0 1 0 9.7070312 15.707031 L 19 6.4140625 L 19 9 A 1.0001 1.0001 0 1 0 21 9 L 21 4.1269531 A 1.0001 1.0001 0 0 0 19.980469 2.9902344 z M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 13 A 1.0001 1.0001 0 1 0 19 13 L 19 19 L 5 19 L 5 5 L 11 5 A 1.0001 1.0001 0 1 0 11 3 L 5 3 z" />
           </svg>
-        </h3>
-      </a>
+        </a>
+      </h3>
       <span class="work-experience-position">{{ experience.position }}</span>
       <div class="work-experience-content">
         <span :key="i" v-for="(c, i) in experience.content">{{ c }}</span>
